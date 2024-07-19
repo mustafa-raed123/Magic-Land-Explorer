3de625ebd355893b9ab7819c3bc5fadcf5cd3fdd
mura123+ <mwsym1898@gmail.com>
2024-07-19 15:54:58 +0300

    Add project files.

---
+++ Magic-Land-Explorer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Magic_Land_Explorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
